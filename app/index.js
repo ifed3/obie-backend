9a4e411055920017249504b79a8bf11fddb5967c
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-09-19 14:34:15 -0700

    Use index file simply for retrieving environment variables

--- app/index.js
+++ app/index.js
@@ -1,19 +1,14 @@
 'use strict';
 
+const extend = require('util')._extend;
 
-// Get libraries
-const express = require('express'),
-    mongoose = require('mongoose'),
-    bodyParser = require('bodyParser'),
-    path = require('path'),
-    secrets = require('./config/secrets');
-
-// Get environment variables
 const development = require('./env/development'),
     test = require('./env/test'),
     production = require('./env/production');
 
-
-const app = express();
-
-module.exports = app;
+// Expose environment variables
+module.exports = {
+    development: extend(development, defaults),
+    test: extend(test, defaults),
+    production: extend(production, defaults)
+}[process.env.NODE_ENV || 'development'];
