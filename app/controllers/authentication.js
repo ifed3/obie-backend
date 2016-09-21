8055a36a73e1afe4b6ae7e76b568c3afe01d7352
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-09-21 04:20:54 -0700

    Flesh out registration, entry and role authorization

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -3,31 +3,70 @@
 const jwt = require('jsonwebtoken'),
     config = require('../../config'),
     expirationSeconds = 60*60*24, // 24 hours in seconds
-    Company = require('../../app/models/company');
+    User = require('../../app/models/user');
 
-exports.create = function(req, res) {
-    Company.findOne({email: req.body.email}, function(err, company) {
-        if (err) res.send(err);
-        if (!company) res.json({success: false, message: 'Authentication failed. Company not found.'});
-        // Company exists in db, so compare passwords and assign token
-        company.comparePassword(req.body.password, function(err, match) {
-            if (err || !match) res.json({ success: false, message: 'Authentication failed. Wrong password'});
-            // Correct company and password params so create token
-            const token = jwt.sign(company, config.secret, {expiresIn: expirationSeconds});
-            res.json({success: true, message: 'Here is the token', token: token});
+function generateToken(user) {
+    return jwt.sign(user, config.secret, {expiresIn: expirationSeconds});
+}
+
+function setUserInfo(req) {
+    return {
+        _id: request._id,
+        name: req.profile.name,
+        email: req.email,
+        role: req.role
+    }
+};
+
+exports.login = function(req, res) {
+    let userInfo = setUserInfo(req.user);
+    res.status(200).json({
+        token: 'JWT ' + generateToken(userInfo), 
+        user: userInfo
+    });
+}
+
+exports.register = function(req, res, next) {
+    const email = req.body.email,
+        name = req.body.name,
+        company = req.body.company,
+        password = req.body.password;
+
+    // Error checking
+    if (!email) return res.status(422).send({ error: 'You must enter an email address'});
+    if (!name) return res.status(422).send({ error: 'You must enter your name'});
+    if (!company) return res.status(422).send({ error: "You must enter your company's name"});
+    if (!password) return res.status(422).send({ error: 'You must enter a password'});
+
+    User.findOne({ email: email }, function(err, currUser) {
+        if (err) return next(err);
+        if (currUser) return res.status(422).send({ error: 'That email address is already in use'});
+
+        // Create a new account when provided with a unique email, and a password
+        let user = new User(req.body);
+        user.save(function(err) {
+            if (err) return next(err);
+            console.log("User succesfully created")
+            // Respond with json web token upon succesfull creation
+            res.status(200).json({
+                token: 'JWT ' + generateToken(userInfo), 
+                user: userInfo
+            });
         });
     });
 }
 
-exports.verify = function(req, res, next) {
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if (token) {
-        jwt.verify(token, config.secret, function(err, decoded) {
-            if (err) return res.json({success: false, message: 'Failed to authenticate token.'});
-            req.decoded = decoded;
-            next()
-        })
-    } else {
-        res.status(403).send({sucess: false, message: 'No token provided'});
+exports.roleAuth = function(role) {
+    return function(req, res, next) {
+        const user = req.user
+        User.findById(user._id, function(err, user) {
+            if (err) {
+                res.status(422).json({ error: 'No user was found'});
+                return next(err);
+            }
+            if (user.role == role) return next();
+            res.status(401).json({ error: "You are not authorized to view this content"});
+            return next('Unauthorized');
+        });
     }
 }
