727347d63ed131fdb96b1da9d9768161228d6184
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-09-21 01:56:23 -0700

    Create app secret for json web token

--- config/env/development.js
+++ config/env/development.js
@@ -4,6 +4,7 @@
 
 module.exports = {
     db: process.env.DEV_MONGODB,
+    secret: process.env.SECRET,
     stripeOptions: {
         apiKey: process.env.DEV_STRIPE_KEY,
         stripePubKey: process.env.DEV_STRIPE_PUB_KEY,
