b5fa6e21742864ab2711d6d7f7dcee83c13a9260
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-09-21 02:02:12 -0700

    Fix password hashing function

--- app/models/company.js
+++ app/models/company.js
@@ -25,10 +25,9 @@ CompanySchema.plugin(stripeCustomer, stripeOptions);
 CompanySchema.pre('save', function(next) {
     var user = this;
     if (!user.isModified('password')) return next();
-
     bcrypt.genSalt(10, function(err, salt) {
         if (err) return next(err);
-        bcrypt.hash(user.password, salt, null, function(err, hash) {
+        bcrypt.hash(user.password, salt, function(err, hash) {
             if (err) return next(err);
             user.password = hash;
             next();
