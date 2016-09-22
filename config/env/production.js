0b78140e6b2e27843a875194b95077e88d02c2bf
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-09-22 06:36:02 -0700

    Update environment variables for production

--- config/env/production.js
+++ config/env/production.js
@@ -3,5 +3,22 @@
 // Expose
 
 module.exports = {
-    db: process.env.PROD_MONGODB
+    db: process.env.PROD_MONGODB,
+    secret: process.env.SECRET,
+    stripeOptions: {
+        apiKey: process.env.PROD_STRIPE_KEY,
+        stripePubKey: process.env.PROD_STRIPE_PUB_KEY,
+        defaultPlan: 'standard',
+        plans: ['standard', 'premium'],
+        planData: {
+            'standard': {
+                name: 'Standard',
+                price: 49
+            },
+            'premium': {
+                name: 'Premium',
+                price: 79
+            }
+        }
+    }
 }
