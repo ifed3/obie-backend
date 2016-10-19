8429d6dc03fa14a60625db95598d8387cc6d277f
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-10-19 14:08:37 -0700

    Remove stripe options for environment config variables

--- config/env/development.js
+++ config/env/development.js
@@ -4,21 +4,22 @@
 
 module.exports = {
     db: process.env.DEV_MONGODB,
-    secret: process.env.SECRET,
-    stripeOptions: {
-        apiKey: process.env.DEV_STRIPE_KEY,
-        stripePubKey: process.env.DEV_STRIPE_PUB_KEY,
-        defaultPlan: 'standard',
-        plans: ['standard', 'premium'],
-        planData: {
-            'standard': {
-                name: 'Standard',
-                price: 49
-            },
-            'premium': {
-                name: 'Premium',
-                price: 79
-            }
-        }
-    }
+    secret: process.env.SECRET
 }
+
+// stripeOptions: {
+//     apiKey: process.env.DEV_STRIPE_KEY,
+//     stripePubKey: process.env.DEV_STRIPE_PUB_KEY,
+//     defaultPlan: 'premium',
+//     plans: ['standard', 'premium'],
+//     planData: {
+//         'standard': {
+//             name: 'Standard',
+//             price: 79
+//         },
+//         'premium': {
+//             name: 'Premium',
+//             price: 129
+//         }
+//     }
+// }
--- config/env/production.js
+++ config/env/production.js
@@ -4,21 +4,22 @@
 
 module.exports = {
     db: process.env.PROD_MONGODB,
-    secret: process.env.SECRET,
-    stripeOptions: {
-        apiKey: process.env.PROD_STRIPE_KEY,
-        stripePubKey: process.env.PROD_STRIPE_PUB_KEY,
-        defaultPlan: 'premium',
-        plans: ['standard', 'premium'],
-        // planData: {
-        //     'standard': {
-        //         name: 'Standard',
-        //         price: 79
-        //     },
-        //     'premium': {
-        //         name: 'Premium',
-        //         price: 129
-        //     }
-        // }
-    }
+    secret: process.env.SECRET
 }
+
+// stripeOptions: {
+//     apiKey: process.env.PROD_STRIPE_KEY,
+//     stripePubKey: process.env.PROD_STRIPE_PUB_KEY,
+//     defaultPlan: 'premium',
+//     plans: ['standard', 'premium'],
+//     planData: {
+//         'standard': {
+//             name: 'Standard',
+//             price: 79
+//         },
+//         'premium': {
+//             name: 'Premium',
+//             price: 129
+//         }
+//     }
+// }
