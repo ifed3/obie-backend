6d9648c38a843d61a0b644a6a9074e769df653ca
Ife Ajiboye <ajiboyeifedayo@gmail.com>
2016-10-19 15:40:33 -0700

    Return stripe access options to config vars

--- config/env/development.js
+++ config/env/development.js
@@ -4,12 +4,13 @@
 
 module.exports = {
     db: process.env.DEV_MONGODB,
-    secret: process.env.SECRET
+    secret: process.env.SECRET,
+    stripeOptions: {
+        apiKey: process.env.DEV_STRIPE_KEY,
+        stripePubKey: process.env.DEV_STRIPE_PUB_KEY
+    }
 }
 
-// stripeOptions: {
-//     apiKey: process.env.DEV_STRIPE_KEY,
-//     stripePubKey: process.env.DEV_STRIPE_PUB_KEY,
 //     defaultPlan: 'premium',
 //     plans: ['standard', 'premium'],
 //     planData: {
--- config/env/production.js
+++ config/env/production.js
@@ -4,12 +4,13 @@
 
 module.exports = {
     db: process.env.PROD_MONGODB,
-    secret: process.env.SECRET
+    secret: process.env.SECRET,
+    stripeOptions: {
+        apiKey: process.env.PROD_STRIPE_KEY,
+        stripePubKey: process.env.PROD_STRIPE_PUB_KEY
+    }
 }
 
-// stripeOptions: {
-//     apiKey: process.env.PROD_STRIPE_KEY,
-//     stripePubKey: process.env.PROD_STRIPE_PUB_KEY,
 //     defaultPlan: 'premium',
 //     plans: ['standard', 'premium'],
 //     planData: {
